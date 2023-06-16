4a3e5467dd94ebb5bc9b842c802fb41d51d4e0fe
Raphael <raphael@wrathcodes.com>
2023-06-16 14:05:30 -0300

    feat: Implementing ... clients update in ClientsRepository and InMemoryClientsRepository

--- src/repositories/in-memory/clients-repository-in-memory.ts
+++ src/repositories/in-memory/clients-repository-in-memory.ts
@@ -22,21 +22,33 @@ export class InMemoryClientsRepository implements ClientsRepository {
 
   async findByEmail(email: string): Promise<Client | null> {
     const client = this.clients.find((client) => client.email === email);
-
-    if (!client) {
-      return null;
-    }
-
+    if (!client) return null;
     return client;
   }
 
   async findById(id: string): Promise<Client | null> {
     const client = this.clients.find((client) => client.id === id);
-
-    if (!client) {
-      return null;
-    }
-
+    if (!client) return null;
     return client;
+  }
+
+  async update(id: string, data: Prisma.ClientUncheckedUpdateInput): Promise<Client> {
+    const client = this.clients.find((client) => client.id === id);
+    if (!client) return null;
+
+    const updatedClient = {
+      ...client,
+      name: data.name || client.name,
+      last_name: data.last_name || client.last_name,
+      email: data.email || client.email,
+      phone: data.phone || client.phone,
+    } as Client;
+
+    this.clients = this.clients.map((client) => {
+      if (client.id === id) return updatedClient;
+      return client;
+    });
+
+    return updatedClient;
   }
 }
--- src/repositories/interfaces/clients-repository.ts
+++ src/repositories/interfaces/clients-repository.ts
@@ -3,4 +3,6 @@ import { Client, Prisma } from "@prisma/client";
 export interface ClientsRepository {
   create(data: Prisma.ClientUncheckedCreateInput): Promise<Client>;
   findByEmail(email: string): Promise<Client | null>;
+  findById(id: string): Promise<Client | null>;
+  update(id: string, data: Prisma.ClientUncheckedUpdateInput): Promise<Client>;
 }
